6337812b1492438d8eb06be2204108a8d518b930
Julio Salvador Girardi <julio.girardi@escola.pr.gov.br>
2026-05-05 12:26:03 +0000

    finalizado

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-let form = document.getElementById('cadastro'); // corrigi o ID do form
-let lista = document.getElementById('listaAluno'); // corrigi o ID da lista
+let form = document.getElementById('cadastro');
+let lista = document.getElementById('listaAluno');
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const nome = document.getElementById('nomeAluno').value;
-    const idade = document.getElementById('idadeAluno').value; // ID correto
-    const login = document.getElementById('loginAluno').value; // ID correto
-    const pizza = document.getElementById('pizzaFavorita').value; // ID correto
-    const SN = document.getElementById('SN').value; // ID correto
-
+    const idade = document.getElementById('idadeAluno').value;
+    const login = document.getElementById('loginAluno').value;
+    const equipe = document.getElementById('equipeFavorita').value;
 
     const novoItem = document.createElement('li');
-    if (SN === "sim"){
-    novoItem.innerHTML = `<strong>${nome}</strong> - ${idade} anos - Login: ${login} - Pizza favorita: ${pizza} - Resposta: ${SN}, Obrigado`;
-    }
-    else{
-        novoItem.innerHTML = `<strong>${nome}</strong> - ${idade} anos - Login: ${login} - Pizza favorita: ${pizza} - Resposta: ${SN}, Vai pro caralho intão. seu miseravel, ridiculo`;
-    }
+
+    novoItem.innerHTML = `<strong>${nome}</strong> - ${idade} anos - Login: ${login} - Equipe Favorita: ${equipe}`;
+
     lista.appendChild(novoItem);
 
     form.reset();
