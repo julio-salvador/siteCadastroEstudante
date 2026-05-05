25be58eaf17fcf06b7d076565c0e796de639f412
Julio Salvador Girardi <julio.girardi@escola.pr.gov.br>
2026-05-05 12:10:17 +0000

    fazer at

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-let form = document.getElementById('cadastro'); 
-let lista = document.getElementById('listaAluno'); 
+let form = document.getElementById('cadastro'); // corrigi o ID do form
+let lista = document.getElementById('listaAluno'); // corrigi o ID da lista
 
 form.addEventListener('submit', function(event){
-   event.preventDefault();
+    event.preventDefault();
 
-   const nome = document.getElementById('nomeAluno').value;
-   const idade = document.getElementById('idadeAluno').value; 
-   const login = document.getElementById('loginAluno').value; 
-   const pizza = document.getElementById('pizzaFavorita').value; 
-   const SN = document.getElementById('SN').value; 
-
-   const novoItem = document.createElement('li');
-   if (SN === "sim"){
-   novoItem.innerHTML = `<strong>${nome}</strong> - ${idade} anos - Login: ${login} - Pizza favorita: ${pizza} - Resposta: ${SN}, Obrigado`;
-   }
-  else{
-       novoItem.innerHTML = `<strong>${nome}</strong> - ${idade} anos - Login: ${login} - Pizza favorita: ${pizza} - Resposta: ${SN}, Vai pro caralho intão. seu miseravel, ridiculo`;
-   }
-   lista.appendChild(novoItem);
+    const nome = document.getElementById('nomeAluno').value;
+    const idade = document.getElementById('idadeAluno').value; // ID correto
+    const login = document.getElementById('loginAluno').value; // ID correto
+    const pizza = document.getElementById('pizzaFavorita').value; // ID correto
+    const SN = document.getElementById('SN').value; // ID correto
 
 
-   form.reset();
+    const novoItem = document.createElement('li');
+    if (SN === "sim"){
+    novoItem.innerHTML = `<strong>${nome}</strong> - ${idade} anos - Login: ${login} - Pizza favorita: ${pizza} - Resposta: ${SN}, Obrigado`;
+    }
+    else{
+        novoItem.innerHTML = `<strong>${nome}</strong> - ${idade} anos - Login: ${login} - Pizza favorita: ${pizza} - Resposta: ${SN}, Vai pro caralho intão. seu miseravel, ridiculo`;
+    }
+    lista.appendChild(novoItem);
+
+    form.reset();
 });
-
-
